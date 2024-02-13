872c380c546a2296d8c5ce23fe837667e7fa4fbe
jasmac <jaskarannagra@hotmail.com>
2024-02-12 23:54:17 -0800

    cert

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,7 +5,7 @@ import "dotenv/config";
 
 let serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 if (process.env.NODE_ENV === "dev") {
