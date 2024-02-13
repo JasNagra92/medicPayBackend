a1d6fa13646160fdd5983fd6fa465b3659bc4754
jasmac <jaskarannagra@hotmail.com>
2024-02-12 23:26:11 -0800

    fixing service account

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,7 +3,11 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import "dotenv/config";
 
-let serviceAccount = require("/app/google-credentials.json");
+let serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
 if (process.env.NODE_ENV === "dev") {
   serviceAccount = require("/Users/jasnagra/downloads/google-credentials.json");
 }
